7d2dad5338148f5afb626eaa846ea854ce518810
vlera-aqifi <150926899+vleraaqifi@users.noreply.github.com>
2024-04-04 11:50:07 +0200

    vlera

--- API/Controllers/ProductsController.cs
+++ API/Controllers/ProductsController.cs
@@ -1,17 +1,18 @@
 using API.Dtos;
+using API.Errors;
 using AutoMapper;
 using Core.Entities;
 using Core.Interfaces;
 using Core.Specifications;
 using Infrastructure.Data;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Http;
 using Microsoft.EntityFrameworkCore;
 
 namespace API.Controllers
 {
-    [ApiController]
-    [Route("api/[controller]")]
-    public class ProductsController : ControllerBase
+   
+    public class ProductsController : BaseApiController
     {
         private readonly IGenericRepository<Product> _productsRepo;
         private readonly IGenericRepository<ProductBrand> _productsBrandRepo;
@@ -31,19 +32,26 @@ namespace API.Controllers
         }
 
         [HttpGet]
-        public async Task<ActionResult<IReadOnlyList<ProductToReturnDto>>> GetProducts()
+        public async Task<ActionResult<IReadOnlyList<ProductToReturnDto>>> GetProducts(string sort)
         {
-            var spec = new ProductsWithTypesAndBrandsSpecification();
+            var spec = new ProductsWithTypesAndBrandsSpecification(sort);
             var products = await _productsRepo.ListAsync(spec);
 
             return Ok(_mapper.Map<IReadOnlyList<Product>,IReadOnlyList<ProductToReturnDto>>(products));
         }
 
         [HttpGet("{id}")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(typeof(ApiResponse),StatusCodes.Status404NotFound)]
         public async Task<ActionResult<ProductToReturnDto>> GetProduct(int id)
         {
              var spec = new ProductsWithTypesAndBrandsSpecification(id);   
             var product = await _productsRepo.GetEntityWithSpec(spec);
+
+            if(product==null){
+                return NotFound(new ApiResponse(404));
+            }
+
             return _mapper.Map<Product, ProductToReturnDto>(product);
         }
         [HttpGet("brands")]
--- Core/Specifications/BaseSpecification.cs
+++ Core/Specifications/BaseSpecification.cs
@@ -20,9 +20,17 @@ namespace Core.Specifications
         public Expression<Func<T, bool>> Criteria {get;}
 
         public List<Expression<Func<T, object>>> Includes {get;} = new List<Expression<Func<T, object>>>();
+        public Expression<Func<T,object>>OrderBy{get;}
+        public Expression<Func<T,object>>OrderByDescending{get;}
 
         protected void AddInclude(Expression<Func<T, object>> includeExpression){
             Includes.Add(includeExpression);
+        }
+        protected void AddOrderBy(Expression<Func<T, object>> orderByExpression){
+            OrderBy = orderByExpression;
+        }
+         protected void AddOrderByDescending(Expression<Func<T, object>> orderbyDescExpression){
+            OrderByDescending = orderbyDescExpression;
         }
     }
 }
